e8d22dc2f9bb247ea979cfa99606e76fddae2c85
Eddymella <edymella2016@gmail.com>
2022-12-14 06:49:27 -0800

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,11 +7,9 @@
 
 void print_alphabet(void)
 {
-	char c;
+	char letter;
 
-	for (c = 'a';  c <= 'z'; c++)
-	{
-	putchar(c);
-	}
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 	_putchar('\n');
 }
